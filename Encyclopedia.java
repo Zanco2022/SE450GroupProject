f0159a77591358cc2ad95fe170be238dd3c13b37
Zanco2022 <100976175+Zanco2022@users.noreply.github.com>
2023-05-16 22:19:39 -0700

    Update Encyclopedia.java

--- Encyclopedia.java
+++ Encyclopedia.java
@@ -1,1 +1,11 @@
+public class Encyclopedia  {
 
+    private String categoryChoice;
+    private String individualTerm;
+
+    public Encyclopedia(String categoryChoice, String individualterm) {
+        this.categoryChoice = categoryChoice;
+        this.individualTerm = individualTerm;
+    }
+
+}
