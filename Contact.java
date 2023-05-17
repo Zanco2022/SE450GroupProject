74e80c63bc8ef7ed12a142792485c518f64bc85d
Zanco2022 <100976175+Zanco2022@users.noreply.github.com>
2023-05-16 23:36:39 -0700

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,8 @@
+public class Contact {
+	private String email;
+	private TextField sendYourMessage;
+	
+	public Contact(email email, TextField sendYourMessage)
+		this.email = email;
+		this.sendYourMessage = sendYourMessage;s
+}
